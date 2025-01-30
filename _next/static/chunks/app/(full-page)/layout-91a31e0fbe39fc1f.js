058978bc6d77a1ddb13fb4556bd569705bd1dd02
serhii-tokranov <serhii-tokranov@users.noreply.github.com>
2025-01-30 11:27:06 +0000

    deploy: enifsoft/au-homer-admin-nextjs@2638bd4f70b2492052a4a8967ec9ebca274f6323

---
+++ _next/static/chunks/app/(full-page)/layout-91a31e0fbe39fc1f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[360],{5317:function(n,e,u){Promise.resolve().then(u.bind(u,5752))}},function(n){n.O(0,[220,568,902,276,752,971,472,744],function(){return n(n.s=5317)}),_N_E=n.O()}]);
